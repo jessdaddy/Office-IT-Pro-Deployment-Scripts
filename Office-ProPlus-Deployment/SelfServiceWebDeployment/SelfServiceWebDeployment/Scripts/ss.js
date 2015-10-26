22b1e4571f12fb0e6f0599576a0b408aff950daf
michaeldraftz <draftz05@gmail.com>
2015-10-25 23:48:39 -0500

    Progress Check in. (extremely basic) Basics of UI and logic in place

---
+++ Office-ProPlus-Deployment/SelfServiceWebDeployment/SelfServiceWebDeployment/Scripts/ss.js
@@ -0,0 +1,50 @@
+﻿var productToInstall = "";
+var versionToInstall = "";
+var languages = "";
+
+function setProduct(product) {
+    productToInstall = product;
+    showLanguageModal();
+}
+
+function setVersion(version) {
+    versionToInstall = version;
+    showProductModal();
+}
+
+function setLanguage() {
+    checkboxes = $(".languageCheckBox").Id;
+    showConfirmationModal();
+}
+
+function startInstall() {
+
+}
+
+function showProductModal() {
+    $("#productModal")[0].style.display = "block";
+    $("#versionModal")[0].style.display = "none";
+    $("#languageModal")[0].style.display = "none";
+    $('#confirmationModal')[0].style.display = "none";
+}
+
+function showLanguageModal() {
+    $("#productModal")[0].style.display = "none";
+    $("#versionModal")[0].style.display = "none";
+    $("#languageModal")[0].style.display = "block";
+    $('#confirmationModal')[0].style.display = "none";
+}
+
+function showVersionModal() {
+    $("#productModal")[0].style.display = "none";
+    $("#versionModal")[0].style.display = "block";
+    $("#languageModal")[0].style.display = "none";
+    $('#confirmationModal')[0].style.display = "none";
+}
+
+function showConfirmationModal() {
+    $("#productModal")[0].style.display = "none";
+    $("#versionModal")[0].style.display = "none";
+    $("#languageModal")[0].style.display = "none";
+    $('#confirmationModal')[0].style.display = "block";
+}
