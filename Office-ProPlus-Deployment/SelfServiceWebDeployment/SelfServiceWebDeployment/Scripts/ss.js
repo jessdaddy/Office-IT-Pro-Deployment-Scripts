eeda0b7aa7be3e62cdf9fafa6e3f3ef4614c61f3
michaeldraftz <draftz05@gmail.com>
2015-10-27 16:49:30 -0500

    Minor tweaks to help readability

--- Office-ProPlus-Deployment/SelfServiceWebDeployment/SelfServiceWebDeployment/Scripts/ss.js
+++ Office-ProPlus-Deployment/SelfServiceWebDeployment/SelfServiceWebDeployment/Scripts/ss.js
@@ -1,6 +1,87 @@
 ﻿var productToInstall = "";
 var versionToInstall = "";
 var languages = "";
+var languageDictionary = {
+    "English": "en-us",
+    "Arabic": "ar-sa",
+    "Bulgarian": "bg-bg",
+    "Chinese (Simplified)": "zh-cn",
+    "Chinese": "zh-tw",
+    "Croatian": "hr-hr",
+    "Czech": "cs-cz",
+    "Croatian": "hr-hr",
+    "Danish": "da-dk",
+    "Estonian": "et-ee",
+    "Finnish": "fi-fi",
+    "French": "fr-fr",
+    "German": "de-de",
+    "Greek": "el-gr",
+    "Hebrew": "he-il",
+    "Hindi": "hi-in",
+    "Hungarian": "hu-hu",
+    "Indonesian": "id-id",
+    "Italian": "it-it",
+    "Japanese": "ja-jp",
+    "Kazakh": "kk-kh",
+    "Korean": "ko-kr",
+    "Latvian": "lv-lv",
+    "Lithuanian": "lt-lt",
+    "Malay": "ms-my",
+    "Norwegian (Bokm�l)": "nb-no",
+    "Polish": "pl-pl",
+    "Portuguese (Brazil)": "pt-br",
+    "Portuguese (Portugal)": "pt-pt",
+    "Romanian": "ro-ro",
+    "Russian": "ru-ru",
+    "Serbian (Latin)": "sr-latn-rs",
+    "Slovak": "sk-sk",
+    "Slovenian": "sl-si",
+    "Spanish": "es-es",
+    "Swedish": "sv-se",
+    "Thai": "th-th",
+    "Turkish": "tr-tr",
+    "Ukrainian": "uk-ua",
+
+    "en-us": "English",
+    "ar-sa": "Arabic",
+    "bg-bg": "Bulgarian",
+    "zh-cn": "Chinese (Simplified)",
+    "zh-tw": "Chinese",
+    "hr-hr": "Croatian",
+    "cs-cz": "Czech",
+    "hr-hr": "Croatian",
+    "da-dk": "Danish",
+    "et-ee": "Estonian",
+    "fi-fi": "Finnish",
+    "fr-fr": "French",
+    "de-de": "German",
+    "el-gr": "Greek",
+    "he-il": "Hebrew",
+    "hi-in": "Hindi",
+    "hu-hu": "Hungarian",
+    "id-id": "Indonesian",
+    "it-it": "Italian",
+    "ja-jp": "Japanese",
+    "kk-kh": "Kazakh",
+    "ko-kr": "Korean",
+    "lv-lv": "Latvian",
+    "lt-lt": "Lithuanian",
+    "ms-my": "Malay",
+    "nb-no": "Norwegian (Bokm�l)",
+    "pl-pl": "Polish",
+    "pt-br": "Portuguese (Brazil)",
+    "pt-pt": "Portuguese (Portugal)",
+    "ro-ro": "Romanian",
+    "ru-ru": "Russian",
+    "sr-latn-rs": "Serbian (Latin)",
+    "sk-sk": "Slovak",
+    "sl-si": "Slovenian",
+    "es-es": "Spanish",
+    "sv-se": "Swedish",
+    "th-th": "Thai",
+    "tr-tr": "Turkish",
+    "uk-ua": "Ukrainian"
+}
 
 function setProduct(product) {
     productToInstall = product;
@@ -15,13 +96,15 @@ function setVersion(version) {
 }
 
 function setLanguage() {
+    var checkboxes = null;
+    languages = null;
     checkboxes = $(".languageCheckBox:checked");
-    $('#productSpan')[0].innerText = checkboxes[0].id;
+    $('#languageSpan')[0].innerText = languageDictionary[checkboxes[0].id];
     languages = [checkboxes[0].id];
     if (checkboxes.length > 1) {
         for (var i = 1; i < checkboxes.length; i++) {
             languages[i] = checkboxes[i].id
-            $('#languageSpan')[0].innerText += ", "+checkboxes[i].id;
+            $('#languageSpan')[0].innerText += ", "+languageDictionary[checkboxes[i].id];
         }
     }
     showConfirmationModal();
@@ -58,3 +141,7 @@ function showConfirmationModal() {
     $("#languageModal")[0].style.display = "none";
     $('#confirmationModal')[0].style.display = "block";
 }
+
+$(document).ready(function () {
+
+});
