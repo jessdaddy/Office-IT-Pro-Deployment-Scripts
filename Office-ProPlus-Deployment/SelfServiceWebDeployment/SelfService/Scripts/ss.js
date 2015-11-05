1e53084838e84215eb2ae9651d7f41c0322153d0
alex182 <alexbaker182@gmail.com>
2015-11-05 14:50:36 -0600

    changed styling of version buttons

--- Office-ProPlus-Deployment/SelfServiceWebDeployment/SelfService/Scripts/ss.js
+++ Office-ProPlus-Deployment/SelfServiceWebDeployment/SelfService/Scripts/ss.js
@@ -180,17 +180,17 @@ function getVersions() {
                         var version = $(this).attr('ID');
                         if (version === "2013") {
                             $('#versions').prepend("<li class='squareButton'>\
-                                        <div class='ms-Dialog-action ms-Button ms-Button--primary ms-bgColor-orangeLight' onclick='setVersion(\"2013\")' style='width:225px;height:250px;padding:50px'>\
-                                        <img src='Content/imgs/office-icon-white.png' style='height:100px'/>\
-                                        <p class='ms-font-xl ms-fontColor-white' style='display:block'>2013</p>\
+                                        <div class='ms-Dialog-action ms-Button ms-Button--primary ms-bgColor-orangeLight version-box' onclick='setVersion(\"2013\")'>\
+                                        <img class='version-img' src='Content/imgs/office-icon-white.png'/>\
+                                        <p class='ms-font-xl ms-fontColor-white version-box-text'>Office 365 ProPlus (2013)</p>\
                                         </div>\
                                         </li>");
                         }
                         if (version === "2016") {
                             $('#versions').prepend("<li class='squareButton'>\
-                                    <div class='ms-Dialog-action ms-Button ms-Button--primary' onclick='setVersion(\"2016\")' style='width:225px;height:250px;padding:50px'>\
-                                    <img src='Content/imgs/office-icon-white.png' style='height:100px'/>\
-                                    <p class='ms-font-xl ms-fontColor-white' style='display:block'>2016</p>\
+                                    <div class='ms-Dialog-action ms-Button ms-Button--primary version-box' onclick='setVersion(\"2016\")'>\
+                                    <img  class='version-img'  src='Content/imgs/office-icon-white.png'>\
+                                    <p class='ms-font-xl ms-fontColor-white'>Office 365 ProPlus (2016)</p>\
                                     </div>\
                                     </li>");
                         }
@@ -199,9 +199,9 @@ function getVersions() {
                 else
                 {
                     $('#versions').prepend("<li class='squareButton'>\
-                                    <div class='ms-Dialog-action ms-Button ms-Button--primary' onclick='setVersion(\"365 ProPlus\")' style='text-align:center;width:225px;height:250px;max-height:250px;padding-top:50px;'>\
-                                    <img src='Content/imgs/office-icon-white.png' style='height:100px;'/>\
-                                    <p class='ms-font-xl ms-fontColor-white' style='display:block;r'>Office 365 ProPlus</p>\
+                                    <div class='ms-Dialog-action ms-Button ms-Button--primary version-box' onclick='setVersion(\"365 ProPlus\")'>\
+                                    <img  class='version-img'  src='Content/imgs/office-icon-white.png'/>\
+                                    <p class='ms-font-xl ms-fontColor-white'>Office 365 ProPlus</p>\
                                     </div>\
                                     </li>");
                 }
