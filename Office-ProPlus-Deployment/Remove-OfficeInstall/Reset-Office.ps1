464c71cc4d3af96a440bd37d626d93b97299c107
unknown <astigall@vcg-adams.vcg.local>
2016-03-07 15:32:29 -0600

    Added reset-office exampel script

---
+++ Office-ProPlus-Deployment/Remove-OfficeInstall/Reset-Office.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+    [string]$targetFilePath = ".\configuration.xml"
+)
+
+Process {
+$scriptPath = "."
+if($PSScriptRoot){
+    $scriptPath = $PSScriptRoot
+}
+else{
+    $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+}
+
+#Importing all required functions
+. $scriptPath\Generate-ODTConfigurationXml.ps1
+. $scriptPath\Nuke-Office-Local.ps1
+. $scriptPath\Install-OfficeClickToRun.ps1
+. $scriptPath\Edit-OfficeConfigurationFile.ps1
+
+Generate-ODTConfigurationXml -Languages AllInUseLanguages -TargetFilePath $targetFilePath
+
+Nuke-Office
+
+Install-OfficeClickToRun -TargetFilePath $targetFilePath
+
+}
