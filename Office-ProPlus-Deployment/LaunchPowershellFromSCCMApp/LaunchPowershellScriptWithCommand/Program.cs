a03ea2987942854bc8f6b841883f05806dfcc685
johnskydiver <jpjones202003@gmail.com>
2016-03-18 11:06:54 -0500

    check-in of using command statement
    This will the use of function calls with arguments

---
+++ Office-ProPlus-Deployment/LaunchPowershellFromSCCMApp/LaunchPowershellScriptWithCommand/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace LaunchPowershellScriptWithCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //couple of things to note about this program
+            //1. The powershell script you wish to run must not require an arguments, and must not require a function call
+            //2. To launch, the call will look like the following: C:\temp>LaunchPowershellScriptWithCommand.exe "& { . .\Change-OfficeChannel.ps1; Change-OfficeChannel -Channel Deferred }"
+            //
+            Process p = new Process();
+            p.StartInfo.FileName = "Powershell.exe";
+            p.StartInfo.Arguments = @"-ExecutionPolicy Bypass -NoExit -Command " + args[0];
+            p.Start();
+            p.Close();
+        }
+    }
+}
